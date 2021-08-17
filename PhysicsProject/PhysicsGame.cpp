fc20c7e7fe9dbc56d89a3a3e32af077d8c45a973
Gage Waguespack <gagewaguespack@gmail.com>
2021-08-17 15:03:27 -0500

    Added PhysicsGame class; Game runs; Nothing pops up on start(on purpose)

---
+++ PhysicsProject/PhysicsGame.cpp
@@ -0,0 +1,18 @@
+#include "PhysicsGame.h"
+
+bool PhysicsGame::startup()
+{
+	return true;
+}
+
+void PhysicsGame::shutdown()
+{
+}
+
+void PhysicsGame::update(float deltaTime)
+{
+}
+
+void PhysicsGame::draw()
+{
+}

74ac420133f2f2eff5d2874ca06869f4484bb99b
Gage Waguespack <gagewaguespack@gmail.com>
2021-08-19 10:14:50 -0500

    Added the scene to PhysicsGame
    (*working*)

--- PhysicsProject/PhysicsGame.cpp
+++ PhysicsProject/PhysicsGame.cpp
@@ -6,10 +6,13 @@
 
 bool PhysicsGame::startup()
 {
-
+	aie::Gizmos::create(255U, 255U, 65535U, 65535U);
 
 	m_renderer = new aie::Renderer2D();
 	setBackgroundColour(0.5f, 0.01f, 0.5f, 0.1f);
+
+	m_scene = new PhysicsScene();
+	m_scene->setTimeStep(0.01f);
 
 	m_font = new aie::Font("../bin/font/consolas.ttf", 32);
 
@@ -19,6 +22,7 @@ bool PhysicsGame::startup()
 void PhysicsGame::shutdown()
 {
 	delete m_renderer;
+	delete m_scene;
 	delete m_font;
 }
 
@@ -26,6 +30,10 @@ void PhysicsGame::update(float deltaTime)
 {
 	//Get the input instance
 	aie::Input* input = aie::Input::getInstance();
+
+	aie::Gizmos::clear();
+	
+	m_scene->update(deltaTime);
 
 	//Exit on Esc
 	if (input->isKeyDown(aie::INPUT_KEY_ESCAPE)) 
@@ -40,10 +48,24 @@ void PhysicsGame::draw()
 
 	m_renderer->begin();
 
+	//Draw the scene
+	m_scene->draw();
+
+	//Draw the Gizmos
+	static float aspectRatio = 16.0f / 9.0f;
+	aie::Gizmos::draw2D(glm::ortho<float>(
+		-100,				//left
+		100,				//right
+		-100 / aspectRatio,	//bottom
+		100 / aspectRatio,	//top
+		-1.0f,				//zNear
+		1.0f				//zFar
+		));
+
 	//Draw FPS
-	m_renderer->setRenderColour(1.0f, 1.0f, 1.0f, 1.0f);
 	char fps[32];
 	sprintf_s(fps, 32, "FPS: %i", getFPS());
+	m_renderer->setRenderColour(1.0f, 1.0f, 1.0f, 1.0f);
 	m_renderer->drawText(m_font, fps, 0.0f, 0.0f);
 
 	m_renderer->end();

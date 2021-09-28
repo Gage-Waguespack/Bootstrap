2dc067870840977ca27a9e15455478bd38a89903
Gage Waguespack <gagewaguespack@gmail.com>
2021-09-28 14:49:11 -0500

    Updated Engine to take in width, height, and name of window
    (*WORKING*)

--- GraphicsProject/Engine.cpp
+++ GraphicsProject/Engine.cpp
@@ -1,9 +1,18 @@
 #include "Engine.h"
+#include "gl_core_4_4.h"
 #include "GLFW/glfw3.h"
+#include <iostream>
 
-Engine::Engine()
+Engine::Engine() : Engine(1280, 720, "Window")
 {
 
+}
+
+Engine::Engine(int width, int height, const char* title)
+{
+	m_width = width;
+	m_height = height;
+	m_title = title;
 }
 
 Engine::~Engine()
@@ -21,7 +30,23 @@ int Engine::run()
 		return exitCode;
 	}
 
+	//Update
+	while (!getGameOver())
+	{
+		exitCode = update();
+		if (exitCode)
+		{
+			return exitCode;
+		}
 
+		exitCode = draw();
+		if (exitCode)
+		{
+			return exitCode;
+		}
+	}
+
+	return 0;
 
 	exitCode = end();
 	if (exitCode) 
@@ -34,21 +59,65 @@ int Engine::run()
 
 int Engine::start()
 {
+	//Initialize GLFW
+	if (!glfwInit())
+	{
+		return -1;
+	}
+
+	//Create a window
+	m_window = glfwCreateWindow(m_width, m_height, m_title, nullptr, nullptr);
+	if (!m_window)
+	{
+		glfwTerminate();
+		return -2;
+	}
+	glfwMakeContextCurrent(m_window);
+
+	//Load OpenGL
+	if (ogl_LoadFunctions() == ogl_LOAD_FAILED)
+	{
+		glfwDestroyWindow(m_window);
+		glfwTerminate();
+		return -3;
+	}
+	int majorVersion = ogl_GetMajorVersion();
+	int minorVersion = ogl_GetMinorVersion();
+	printf("OpenGL version %i.%i\n", majorVersion, minorVersion);
 	return 0;
 }
 
 int Engine::update()
 {
+	if (!m_window) return -4;
+
+	glfwPollEvents();
+
+	return 0;
+}
+
+int Engine::draw()
+{
+	if (!m_window) return -5;
+
+	glfwSwapBuffers(m_window);
 	return 0;
 }
 
 int Engine::end()
 {
+	if (!m_window) return -6;
+
+	glfwDestroyWindow(m_window);
+	glfwTerminate();
+
 	return 0;
 }
 
 bool Engine::getGameOver()
 {
+	if (!m_window) return true;
+
 	bool gameIsOver = glfwWindowShouldClose(m_window);
 	gameIsOver = gameIsOver || glfwGetKey(m_window, GLFW_KEY_ESCAPE) == GLFW_PRESS;
 
